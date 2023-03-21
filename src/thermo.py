dc3c35b5603aaf2bd58b902b2480f02c93163a8e
Calvin Pieters <calvinpieters@gmail.com>
2023-03-21 15:57:52 +0200

    Z roots

--- src/thermo.py
+++ src/thermo.py
@@ -18,17 +18,24 @@ def ideal_gas_law(gas_constant: float, list_of_volumes: List, list_of_temperatur
     return df
 
 
-# Plot Function for Task 1 - Ideal Gas Law
+# Plot Function for Task 2 - Ideal Gas Law
 
 def plot_ideal_gas_law(ideal_gas_law_df: pd.DataFrame) -> None:
     for column in ideal_gas_law_df:
         plt.plot(ideal_gas_law_df.index, ideal_gas_law_df[column], label=column)
         
 
-# Plot Function for Task 3 - Ideal Gas Law
+# Peng-Robinson EOS Function for Task 3
 
 def peng_robinson_eos(gas_constant: float, list_of_volumes: List, list_of_temperatures: List, bc: float, ac: float, alpha: Callable) -> pd.DataFrame:
     df = pd.DataFrame(columns=list_of_temperatures, index=list_of_volumes)
     temperature_aT = {T: (ac * alpha(T)) for T in list_of_temperatures}
     df = df.apply(lambda x: ((gas_constant * float(x.name)) / (x.index - bc)) - (temperature_aT[float(x.name)] / ((x.index * (x.index + bc) + bc * (x.index - bc)))), axis=0)
     return df
+
+# Plot Function for Task 4 - Peng-Robinson EOS
+
+def plot_peng_robinson_eos(peng_robinson_eos_df: pd.DataFrame) -> None:
+    for column in peng_robinson_eos_df:
+        plt.plot(peng_robinson_eos_df.index, peng_robinson_eos_df[column], label=column)
+
